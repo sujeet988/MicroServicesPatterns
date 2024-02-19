32016d547698ad002bdf85c2cca539f66f84386d
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-02-19 08:56:05 +0530

    Add project files.

---
+++ CircuitBreakerWithOutPolly/CircuitBreaker.cs
@@ -0,0 +1,43 @@
+﻿namespace CircuitBreakerWithOutPolly
+{
+    public  class CircuitBreaker
+    {
+        private readonly TimeSpan _timeout;
+        private readonly int _retryLimit;
+        private int _retryCount;
+        private DateTime _lastFailedAttempt;
+        public CircuitBreaker(TimeSpan timeout, int retryLimit)
+        {
+            _timeout = timeout;
+            _retryLimit = retryLimit;
+            _retryCount = 0;
+            _lastFailedAttempt = DateTime.MinValue;
+
+        }
+        public bool IsCircuitOpen
+        {
+            get
+            {
+                if (_retryCount < _retryLimit) return false;
+                if (DateTime.UtcNow - _lastFailedAttempt > _timeout)
+                {
+                    _retryCount = 0;
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public void RecordFailure()
+        {
+            _retryCount++;
+            _lastFailedAttempt = DateTime.UtcNow;
+        }
+
+        public void Reset()
+        {
+            _retryCount = 0;
+        }
+
+    }
+}
