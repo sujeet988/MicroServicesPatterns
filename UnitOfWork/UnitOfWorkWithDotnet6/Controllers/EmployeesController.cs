e9fbfcf18d0054ae9b5228883497fcd84f5f3b9e
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-02-20 10:51:51 +0530

    Unit of Work Demo

--- UnitOfWork/UnitOfWorkWithDotnet6/Controllers/EmployeesController.cs
+++ UnitOfWork/UnitOfWorkWithDotnet6/Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using UnitOfWorkWithDotnet6.Models;
 using UnitOfWorkWithDotnet6.UOW;
 
@@ -12,19 +13,31 @@ namespace UnitOfWorkWithDotnet6.Controllers
     {
         //The following variable will hold the IUnitOfWork Instance
         private readonly IUnitOfWork _unitOfWork;
+        protected ResponseDto _response;
 
         public EmployeesController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            _response = new ResponseDto();
         }
 
         // GET: Employees
         [HttpGet(Name = "GetEmpNames")]
-        public async Task<IActionResult> GetEmpNames()
+        public async Task<object> GetEmpNames()
         {
-            //Use Employee Repository to Fetch all the employees along with the Department Data
-            var employees = await _unitOfWork.Employees.GetAllEmployeesAsync();
-            return Ok(employees);
+
+            try
+            {
+                var employees = await _unitOfWork.Employees.GetAllEmployeesAsync();
+                _response.Result = employees;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
         }
 
         // GET: Employees/Details/5
@@ -48,9 +61,9 @@ namespace UnitOfWorkWithDotnet6.Controllers
         // POST: Employees/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost("CreateEmpdata")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([FromBody] Employee employee)
+        public async Task<object> Create([FromBody] Employee employee)
         {
             if (ModelState.IsValid)
             {
@@ -66,27 +79,90 @@ namespace UnitOfWorkWithDotnet6.Controllers
                     await _unitOfWork.Save();
                     //Commit the Changes to database
                     _unitOfWork.Commit();
+                    _response.Result = employee;
 
 
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
                     //Rollback Transaction
                     _unitOfWork.Rollback();
                     //Log The Exception
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages = new List<string>() { ex.ToString() };
                 }
             }
+            return _response;
 
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit([FromBody] Employee employee)
+        public async Task<object> Edit(int id, [FromBody] Employee employee)
         {
-           
+            if (id != employee.EmployeeId)
+            {
+                return NotFound();
+            }
 
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //Begin The Tranaction
+                    _unitOfWork.CreateTransaction();
+                    //Use Generic Reposiory to Insert a new employee
+                    await _unitOfWork.Employees.UpdateAsync(employee);
+                    //Save Changes to database
+                    await _unitOfWork.Save();
+                    //Commit the Changes to database
+                    _unitOfWork.Commit();
+                    _response.Result = employee;
+
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    //Rollback Transaction
+                    _unitOfWork.Rollback();
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages = new List<string>() { ex.ToString() };
+                }
+            }
+            return _response;
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            //Begin The Tranaction
+            _unitOfWork.CreateTransaction();
+            var employee = await _unitOfWork.Employees.GetByIdAsync(id);
+            if (employee != null)
+            {
+                try
+                {
+                    await _unitOfWork.Employees.DeleteAsync(id);
+                    //Save Changes to database
+                    await _unitOfWork.Save();
+                    //Commit the Changes to database
+                    _unitOfWork.Commit();
+                    _response.Result = id;
+                }
+                catch (Exception ex)
+                {
+                    //Rollback Transaction
+                    _unitOfWork.Rollback();
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages = new List<string>() { ex.ToString() };
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
-
-
-
 }
+
+
+
+
