71b221ef0f64689e1392ed7460dc1f879ecd694c
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-04-21 07:56:53 +0530

    Design pattern added for composite

---
+++ CompositeDesignPattern/FileSystemUsingCompositeDesignPattern/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileSystemUsingCompositeDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
