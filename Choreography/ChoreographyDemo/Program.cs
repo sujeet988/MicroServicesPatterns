cbc43cc4e2d7deb167887a227ae022a882595a6f
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-04-21 14:54:53 +0530

    Some Pattern Added

---
+++ Choreography/ChoreographyDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChoreographyDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
