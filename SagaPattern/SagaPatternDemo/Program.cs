bb6442ea785e283b8ca323ed027a6c237f7758d4
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-04-21 07:53:17 +0530

    Saga Pattern Added

---
+++ SagaPattern/SagaPatternDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SagaPatternDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
