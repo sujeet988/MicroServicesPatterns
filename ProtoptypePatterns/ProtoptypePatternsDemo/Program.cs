ea1d78c18ba2278b6fc75e1da4c8719055d82f48
Sujeet Kumar <sujeetkumar@microsoft.com>
2024-04-28 11:30:17 +0530

    ProotoType Design Patterns

---
+++ ProtoptypePatterns/ProtoptypePatternsDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ProtoptypePatternsDemo
+{
+    public class Student
+    {
+        public int age;
+        public string name;
+        private int rollNumber;
+        
+        public Student()
+        {
+
+        }
+       public Student(int age, int rollNumber, string name) 
+        {
+        this.age = age;
+        this.rollNumber = rollNumber;
+        this.name = name;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Protoptype problme demo");
+            Student obj=new Student(29,70,"sujeet");
+
+            // Creating clone of object.
+            Student cloneObject=new Student();
+            cloneObject.age = obj.age;
+            cloneObject.name = obj.name;
+            //cloneObject.rollNumber=obj.rollNumber // it will give compile time error
+                     //  as it is private. for fixing this issues we using prototype pattern.
+                     // and we moving clone logic even in parent class/interfacee
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProtoptypePatterns/ProtoptypePatternsFixedIssues/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ProtoptypePatternsFixedIssues
+{
+    public interface IProtoType
+    {
+        IProtoType Clone();
+    }
+    public class Student : IProtoType
+    {
+        public int age;
+        public string name;
+        private int rollNumber;
+
+        public Student()
+        {
+
+        }
+        public Student(int age, int rollNumber, string name)
+        {
+            this.age = age;
+            this.rollNumber = rollNumber;
+            this.name = name;
+        }
+
+        public IProtoType Clone()
+        {
+            return new Student(age, rollNumber, name);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Protoptype Patterns Fixed Issues Demo");
+            // first create original object
+            Student obj = new Student(29,70,"sujeet");
+            Console.WriteLine("orignal Name");
+            Console.WriteLine(obj.name);
+            // cloned object
+            Student cloned =(Student)obj.Clone();
+            // print cloned data
+            Console.WriteLine("Cloned Names");
+            Console.WriteLine(cloned.name);
+
+            Console.ReadLine();
+        }
+    }
+}
