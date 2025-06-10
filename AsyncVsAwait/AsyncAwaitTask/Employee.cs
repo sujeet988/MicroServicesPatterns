2f5dcf45d401021ef6e4906af7043d643827946d
DELL <DELL@DESKTOP-VLVF8CL>
2025-06-10 16:15:19 +0530

    commit the async and await

---
+++ AsyncVsAwait/AsyncAwaitTask/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitTask
+{
+    public class Employee
+    {
+        public async Task<string> GetEmployeeNameAsync()
+        {
+            // Simulate an asynchronous operation
+            await Task.Delay(6000);
+            return "John Doe";
+        }
+        public async Task<int> GetEmployeeId(int empid)
+        {
+            // Simulate an asynchronous operation
+            await Task.Delay(6000);
+            return empid;
+        }
+    }
+}
---
+++ AsyncVsAwait/AsyncAwaitTask/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace AsyncAwaitTask
+{
+    internal class Program
+    {
+
+        // Thread -blocking
+        // parallel execution
+        public async Task GetDataParallelwithoutawait()
+        {
+            Employee emp = new Employee();
+
+            var nameTask = emp.GetEmployeeNameAsync();
+            var idTask = emp.GetEmployeeId(101);
+
+
+            Console.WriteLine($"Employee Name: {nameTask.Result}, Employee ID: {idTask.Result}");
+            Console.WriteLine("ended");
+        }
+
+        //thread-blocking
+        // parallel execution
+        public void GetDataParallel()
+        {
+            Employee emp = new Employee();
+            var nameTask = emp.GetEmployeeNameAsync();
+            var idTask = emp.GetEmployeeId(101);
+
+            // Wait for both tasks to complete
+            Task.WaitAll(nameTask, idTask);
+
+            Console.WriteLine($"Employee Name: {nameTask.Result}, Employee ID: {idTask.Result}");
+        }
+        //Not thread-blocking
+        public async void GetDataNotParallel()
+        {
+            Employee emp = new Employee();
+            var nameTask = await emp.GetEmployeeNameAsync();
+            Console.WriteLine($"Employee Name: {nameTask}");
+
+            if (nameTask != null)
+            {
+                var idTask = await emp.GetEmployeeId(101);
+                Console.WriteLine($"Employee id: {nameTask}");
+            }
+        }
+        public async Task GetDataParallelAsync()
+        {
+            Employee emp = new Employee();
+
+            var nameTask = emp.GetEmployeeNameAsync();
+            var idTask = emp.GetEmployeeId(101);
+
+            await Task.WhenAll(nameTask, idTask);
+
+            Console.WriteLine($"Employee Name: {nameTask.Result}, Employee ID: {idTask.Result}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
