9d12dcb420f58723e86a2112af1dfa4fd2c435b3
DELL <DELL@DESKTOP-VLVF8CL>
2025-06-10 11:25:13 +0530

    Unit test code chnages done

--- UnitTestsWIthMsTest/FactoryDesignPatternsUnitTestsWIthMsTest/UnitTest1.cs
+++ UnitTestsWIthMsTest/FactoryDesignPatternsUnitTestsWIthMsTest/UnitTest1.cs
@@ -1,11 +1,34 @@
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FactoryDesignPatterns;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
 namespace FactoryDesignPatternsUnitTestsWIthMsTest
 {
     [TestClass]
-    public class UnitTest1
+    public class TransportFactoryTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        ITransport transport = null;
+        public TransportFactoryTests()
         {
+           
+        }
+
+        [TestMethod]
+        public void GetTransport_ShouldReturnCar_WhenTypeIsCar()
+        {
+            transport = TransportFactory.GetTransport("Car");
+            Assert.IsNotNull(transport);
+            Assert.IsInstanceOfType(transport, typeof(Car));
+        }
+
+        [TestMethod]
+        public void GetTransport_ShouldReturnException_WhenTypeIsInValid()
+        {
+          
+            var ex= Assert.ThrowsException<ArgumentException>( () => TransportFactory.GetTransport("InValid"));
+            Assert.AreEqual("Invalid transport type", ex.Message);
         }
     }
 }
