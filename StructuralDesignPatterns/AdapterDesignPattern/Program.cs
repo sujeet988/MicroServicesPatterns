17602599d4762f157bfce602af0540b65f1cf2a6
DELL <DELL@DESKTOP-VLVF8CL>
2025-06-10 10:43:53 +0530

    Adaper Design Patterns Aded

--- StructuralDesignPatterns/AdapterDesignPattern/Program.cs
+++ StructuralDesignPatterns/AdapterDesignPattern/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace AdapterDesignPatternDemo
+﻿namespace AdapterDesignPattern
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Adapter Design Patterns");
         }
     }
 }
